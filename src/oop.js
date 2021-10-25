a13c85d38bf8b42f62da9b80e44f5029489489d5
FurkanARTAR <asosyalgenctvtr@gmail.com>
2021-10-25 12:14:45 +0300

    first commit

---
+++ src/oop.js
@@ -0,0 +1,35 @@
+//Değer atamasını prototype yapısı ile yapıyoruz.
+
+export class Customer {
+    constructor(id, customerNumber) {
+      this.id = id;
+      this.customerNumber=customerNumber
+    }
+  }
+  
+  let customer = new Customer(1, "12345");
+  customer.a = "Kadir Demirel";
+  console.log(customer.a);
+  
+  Customer.bisey = "Bişey";
+  console.log(Customer.bisey);
+  console.log(customer.customerNumber)
+  
+  
+  class IndividualCustomer extends Customer
+  {
+      constructor(firstName,id,customerNumber)
+      {
+          super(id,customerNumber)
+          this.firstName=firstName
+      }
+  }
+  
+  class CorporateCustomer extends Customer
+  {
+  constructor(companyName,id,customerNumber)
+  {
+      super(id,customerNumber)
+      this.companyName=companyName
+  }
+  }
